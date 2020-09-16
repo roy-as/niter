a4d399366a2d926cc71b035c8979cb2030fae503
aby <qp-roy@ying31.com>
2020-09-16 18:06:08 +0800

    修改新闻更新时间

--- src/main/java/cn/niter/forum/schedule/NewsUpdateTasks.java
+++ src/main/java/cn/niter/forum/schedule/NewsUpdateTasks.java
@@ -16,7 +16,7 @@ public class NewsUpdateTasks {
     @Autowired
     private AliProvider aliProvider;
 
-    @Scheduled(cron = "0 7 10 * * ?") //每天10点7分更新
+    @Scheduled(cron = "0 15 18 * * ?") //每天10点7分更新
     public void updateDiannaoNewsSchedule() {
         log.info("updateDiannaoNewsSchedule start {}", new Date());
         aliProvider.autoGetNews(NewsColumnEnum.NEWS_COLUMN_DIANNAO.getStrId(),1);
@@ -28,19 +28,19 @@ public class NewsUpdateTasks {
         aliProvider.autoGetNews(NewsColumnEnum.NEWS_COLUMN_GUONEI.getStrId(),20);
         log.info("updateGuoneiNewsSchedule stop {}", new Date());
     }
-    @Scheduled(cron = "0 8 11 * * ?") //每天11点8分更新
+    @Scheduled(cron = "0 18 18 * * ?") //每天11点8分更新
     public void updateHulianwangNewsSchedule() {
         log.info("updateHulianwangNewsSchedule start {}", new Date());
         aliProvider.autoGetNews(NewsColumnEnum.NEWS_COLUMN_HULIANWANG.getStrId(),1);
         log.info("updateHulianwangNewsSchedule stop {}", new Date());
     }
-    @Scheduled(cron = "0 9 12 * * ?") //每天12点9分更新
+    @Scheduled(cron = "0 21 18 * * ?") //每天12点9分更新
     public void updateKejiNewsSchedule() {
         log.info("updateKejiNewsSchedule start {}", new Date());
         aliProvider.autoGetNews(NewsColumnEnum.NEWS_COLUMN_KEJI.getStrId(),1);
         log.info("updateKejiNewsSchedule stop {}", new Date());
     }
-    @Scheduled(cron = "0 10 13 * * ?")//每天13点10分更新
+    @Scheduled(cron = "0 23 18 * * ?")//每天13点10分更新
     public void updateKepuNewsSchedule() {
         log.info("updateKepuNewsSchedule start {}", new Date());
         aliProvider.autoGetNews(NewsColumnEnum.NEWS_COLUMN_KEPU.getStrId(),1);
